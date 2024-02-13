aec5fa229f978a7bef1c17a7eee71eb561592d2a
ostweg <hoechstesetablissement@gmail.com>
2024-02-13 15:40:33 +0100

    basic story #1

--- stockrecolib/chatbot/actions/actions.py
+++ stockrecolib/chatbot/actions/actions.py
@@ -1,27 +1,24 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ExtractStockEntity(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_extract_stock_entity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        stock_entity = next(tracker.get_latest_entity_values('stock'),None)
+        
+        print(f"****____{stock_entity}_____*********")
+
+        if stock_entity:
+            dispatcher.utter_message(text=f"Will retrieve data for '{stock_entity}!")
+        else:
+            dispatcher.utter_message(text="I didn't get the stock name. What do you wanna do?")
+        return []
