3ba7e4d684f0902d638da3ba512d7de2e769d3f2
ostweg <hoechstesetablissement@gmail.com>
2024-04-02 20:25:50 +0200

    #1 Added macd

--- stockrecolib/chatbot/actions/StockCalculator.py
+++ stockrecolib/chatbot/actions/StockCalculator.py
@@ -17,3 +17,14 @@ class StockCalculator:
         ema_down = down.ewm(com=14 -1, adjust=False).mean()
         rs = ema_up / ema_down
         return str(100 - (100 / (1+rs)).iloc[-1])
+    
+    def calculate_MACD(self):
+        data = yf.Ticker(self.ticker).history(period='1y').Close
+        short_EMA = data.ewm(span=12, adjust=False).mean()
+        long_EMA = data.ewm(span=12, adjust=False).mean()
+
+        MACD = short_EMA - long_EMA
+        signal = MACD.ewm(span=9, adjust=False).mean()
+        MACD_histogram = MACD - signal
+
+        return f'{MACD[-1]}, {signal[-1]}, {MACD_histogram[-1]}'
--- stockrecolib/chatbot/actions/actions.py
+++ stockrecolib/chatbot/actions/actions.py
@@ -38,7 +38,27 @@ class ExtractSockRSI(Action):
             
             sc = StockCalculator(stock_entity)
 
-            dispatcher.utter_message(text=f"The current stock rsi for {stock_entity} is {sc.calculate_RSI()}")
+            dispatcher.utter_message(text=f"The current RSI for {stock_entity} is {sc.calculate_RSI()}")
+        else:
+            dispatcher.utter_message(text="I didn't get the stock name. What do you wanna do?")
+        return []
+    
+class ExtractSockMACD(Action):
+
+    def name(self) -> Text:
+        return "action_extract_stock_macd"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        stock_entity = next(tracker.get_latest_entity_values('stock'),None)
+        
+        if stock_entity:
+            
+            sc = StockCalculator(stock_entity)
+
+            dispatcher.utter_message(text=f"The current MACD for {stock_entity} is {sc.calculate_MACD()}")
         else:
             dispatcher.utter_message(text="I didn't get the stock name. What do you wanna do?")
         return []
